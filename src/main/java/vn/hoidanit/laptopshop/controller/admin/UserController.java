cebb3c7dcdde6efeae84fb55098cbc4f72dfda70
Sang <buiquangsang123456@gmail.com>
2024-11-29 20:24:56 +0700

    sfnsjkdfnsd

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -0,0 +1,106 @@
+package vn.hoidanit.laptopshop.controller.admin;
+
+import java.util.List;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletContext;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UploadService;
+import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+    private final UploadService uploadService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserService userService, ServletContext servletContext, UploadService uploadService,
+            BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.uploadService = uploadService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        List<User> arrUsers = this.userService.getAllUserByEmail("sang@gmail.com");
+        System.out.println(arrUsers);
+        model.addAttribute("eric", "test");
+        model.addAttribute("hoidanit", "from controller");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUser();
+        model.addAttribute("users", users);
+        return "admin/user/show";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "admin/user/detail";
+    }
+
+    @GetMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @PostMapping("/admin/user/create")
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit,
+            @RequestParam("sangFile") MultipartFile file) {
+        String avatar = this.uploadService.handleSaveUploadfile(file, "avatar");
+        String hashPassword = this.bCryptPasswordEncoder.encode(hoidanit.getPassword());
+        // this.userService.handleSaveUser(hoidanit);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping("/admin/user/update/{id}")
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
+        User currentUser = this.userService.getUserById(id);
+        model.addAttribute("currentUser", currentUser);
+        return "admin/user/update";
+    }
+
+    @PostMapping("/admin/user/update")
+    public String postUpdateUser(Model model, @ModelAttribute("currentUser") User user) {
+        User currentUser = this.userService.getUserById(user.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(user.getAddress());
+            currentUser.setFullName(user.getFullName());
+            currentUser.setPhone(user.getPhone());
+            this.userService.handleSaveUser(currentUser);
+        }
+        return "redirect:/admin/user";
+    }
+
+    @GetMapping("/admin/user/delete/{id}")
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
+        // model.addAttribute("id", id);
+        // User user = new User();
+        // user.setId(id);
+        model.addAttribute("deleteUser", new User());
+        return "admin/user/delete";
+    }
+
+    @PostMapping("/admin/user/delete")
+    public String postDeleteUser(Model model, @ModelAttribute("deleteUser") User user) {
+        this.userService.deleteById(user.getId());
+        return "redirect:/admin/user";
+    }
+
+}
